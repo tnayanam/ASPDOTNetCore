21bfd1a35d25fb06f9aaba8fa58776aa3c7f9438
Tanuj Nayanam <tnayanam@sessionm.com>
2019-02-08 16:00:01 -0500

    Dependency Injection DI of services and interfaces

--- GlobeMantics/GlobeMantics/Startup.cs
+++ GlobeMantics/GlobeMantics/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Globemantics.Services;
+using GlobeMantics.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +17,8 @@ namespace GlobeMantics
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IConferenceService, ConferenceMemoryService>(); // Dependency Injection
+            services.AddSingleton<IProposalService, ProposalMemoryService>(); // Dependency Injection
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,3 +36,6 @@ namespace GlobeMantics
         }
     }
 }
+
+// Now we need to tell that whenever I am using the Interface of proposal and Conference I need to pass Object instance of Conference Memory Service and Proposal Memory Service.
+
