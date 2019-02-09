fce8badb387ab50f6c1c18ce1561db45412720cf
Tanuj Nayanam <tnayanam@sessionm.com>
2019-02-09 01:52:33 -0500

    Bundle and Minify

--- GlobeMantics/GlobeMantics/Program.cs
+++ GlobeMantics/GlobeMantics/Program.cs
@@ -28,3 +28,10 @@ namespace GlobeMantics
 // file, and then type the bootstrap thing as seen in packae.json file. the bootstrap gets installed as soon as you save
 // the file, also when you see the hidden files from solution explorer you cn see a folder called node_modules
 // where you can see bootstarp files, just copy the file syou need a paste it in the wwwroot folder.
+
+    /*
+     *Suppose you want to deploy minified version, jsut create a json file and add it in the solution
+     * directory and then name it as bundleconfig.json and add the content as shown in that file. and then
+     * go to view -> other windowws -> task runner and then clikc on Update all files and you should see those
+     * files in wwwroot folder.
+     */
