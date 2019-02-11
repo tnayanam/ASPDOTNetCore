dcff58ced0b407f69fb9b98035438b004d0a94b9
Tanuj Nayanam <tnayanam@sessionm.com>
2019-02-10 19:36:04 -0500

    Controller Added

---
+++ GlobeMantics/GlobeMantics/Controllers/ConferenceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Globemantics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace GlobeMantics.Controllers
+{
+    public class ConferenceController:Controller
+    {
+        private IConferenceService service;
+
+        public ConferenceController(IConferenceService service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await service.GetAll());
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Title = "Add Conference";
+            return View(new ConferenceModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ConferenceModel model)
+        {
+            if (ModelState.IsValid)
+                await service.Add(model);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ GlobeMantics/GlobeMantics/Controllers/ProposalController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Globemantics.Services;
+using GlobeMantics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace GlobeMantics.Controllers
+{
+    public class ProposalController : Controller
+    {
+        private IConferenceService conferenceService;
+        private IProposalService proposalService;
+
+        public ProposalController(IConferenceService conferenceService, IProposalService proposalService)
+        {
+            this.conferenceService = conferenceService;
+            this.proposalService = proposalService;
+        }
+
+        public async Task<IActionResult> Index(int conferenceId)
+        {
+            var conference = await conferenceService.GetById(conferenceId);
+            ViewBag.Title = $"Proposal for Conference {conference.Name} at location {conference.Location}";
+            ViewBag.ConferenceId = conferenceId;
+
+            return View(await (proposalService.GetAll(conferenceId)));
+        }
+
+        public IActionResult Add(int conferenceId)
+        {
+            ViewBag.Title = "Add Proposal";
+            return View(new ProposalModel {ConferenceId = conferenceId});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProposalModel proposal)
+        {
+            if (ModelState.IsValid)
+                await proposalService.Add(proposal);
+            return RedirectToAction("Index", new {conferenceId = proposal.ConferenceId});
+        }
+
+        public async Task<IActionResult> Approve(int proposalId)
+        {
+            var proposal = await proposalService.Approve(proposalId);
+            return RedirectToAction("Index", new {conferenceId = proposal.ConferenceId});
+        }
+    }
+}
